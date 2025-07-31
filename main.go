9f2cb4e78b608f30c8d5edfa34b068451b3bd04a
Aji Ramadhani Setianto <ajiramadhani56@gmail.com>
2025-07-31 20:33:23 +0700

    penambahan put, save data by file

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gorilla/mux"
@@ -33,6 +35,9 @@ func createTodo(w http.ResponseWriter, r *http.Request) {
 	todo.ID = nextID
 	nextID++
 	todos = append(todos, todo)
+
+	saveTodosToFile()
+
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(todo)
 }
@@ -49,6 +54,9 @@ func deleteTodo(w http.ResponseWriter, r *http.Request) {
 	for i, t := range todos {
 		if t.ID == id {
 			todos = append(todos[:i], todos[i+1:]...)
+
+			saveTodosToFile()
+
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
@@ -57,12 +65,65 @@ func deleteTodo(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Todo not found", http.StatusNotFound)
 }
 
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var updateTodo Todo
+	err = json.NewDecoder(r.Body).Decode(&updateTodo)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	for i, t := range todos {
+		if t.ID == id {
+			todos[i].Task = updateTodo.Task
+
+			saveTodosToFile()
+
+			json.NewEncoder(w).Encode(todos[i])
+			return
+		}
+	}
+
+	http.Error(w, "Todo not found", http.StatusNotFound)
+}
+
+func saveTodosToFile() {
+	data, _ := json.MarshalIndent(todos, "", "  ")
+	ioutil.WriteFile("data.json", data, 0644)
+}
+
+func loadTodosFromFile() {
+	file, err := os.ReadFile("data.json")
+	if err != nil {
+		return
+	}
+	json.Unmarshal(file, &todos)
+
+	// hitung nextID berdasarkan data terakhir
+	for _, t := range todos {
+		if t.ID >= nextID {
+			nextID = t.ID + 1
+		}
+	}
+}
+
 func main() {
+	loadTodosFromFile()
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/todos", getTodos).Methods("GET")
 	r.HandleFunc("/todos", createTodo).Methods("POST")
 	r.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
+	r.HandleFunc("/todos/edit/{id}", updateTodo).Methods("PUT")
 
 	fmt.Println("Server running at http://localhost:8080")
 	http.ListenAndServe(":8080", r)
